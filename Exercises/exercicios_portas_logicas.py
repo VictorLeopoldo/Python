e5cb4db86302052a1392f24db0f77d5e1592037e
victor <victorleovazavi@gmail.com>
2026-03-05 22:05:02 -0300

    Add Exercicio if \ else

--- Exercises/exercicios_portas_logicas.py
+++ Exercises/exercicios_portas_logicas.py
@@ -10,3 +10,23 @@ elif n1 == n2:
     print(f"Os dois números são iguais")
 else:
     print(f"O número maior é {n2}")
+"""
+2-Faça um programa que leia um número inteiro fornecido pelo usuário. 
+Se esse número for positivo, calcule a raiz quadrada do número e apresente-a. Se o número for negativo,
+mostre uma mensagem dizendo que o número é inválido.
+"""
+num = int(input("Informe um número inteiro: "))
+if num >= 0:
+    quad = num ** 0.5
+    print(f"A raiz quadrada de {num} é {quad}")
+else:
+    print("O número informado é negativo")
+
+"""
+3-Faça um programa que recebe um número inteiro e informe se este número é par ou ímpar
+"""
+nb = int(input("Digite um número inteiro: "))
+if nb % 2 == 0:
+    print(f"O número {nb} é par")
+else:
+    print(f"O número {nb} é ímpar")
