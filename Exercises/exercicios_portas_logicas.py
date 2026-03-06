ad16bbb1f761d05d6aec1ee261521b6b5625b8f4
victor <victorleovazavi@gmail.com>
2026-03-05 21:49:01 -0300

    Exercicio 1 - Lógica

---
+++ Exercises/exercicios_portas_logicas.py
@@ -0,0 +1,12 @@
+"""
+1- Faça um programa que receba dois números inteiros e mostre qual deles é o maior:
+"""
+n1 = int(input("Digite o primeiro número inteiro: "))
+n2 = int(input("Digite o segundo número inteiro: "))
+
+if n1 > n2:
+    print(f"O maior número é o {n1}")
+elif n1 == n2:
+    print(f"Os dois números são iguais")
+else:
+    print(f"O número maior é {n2}")
