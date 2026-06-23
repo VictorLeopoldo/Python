7a9a0a7325314593150ebe66b0e50e818a4c608a
victor <victorleovazavi@gmail.com>
2026-06-22 22:31:25 -0300

    exerciciofor

--- Exercises/exercicioa_if_elif_else1.py
+++ Exercises/exercicioa_if_elif_else1.py
@@ -1,8 +1,8 @@
 """
 1- Faça um programa que receba dois números inteiros e mostre qual deles é o maior:
 """
-n1 = int = int(input("Digite o primeiro número inteiro: "))
-n2 = int = int(input("Digite o segundo número inteiro: "))
+n1 = int(input("Digite o primeiro número inteiro: "))
+n2 = int(input("Digite o segundo número inteiro: "))
 
 if n1 > n2:
     print(f"O maior número é o {n1}")
---
+++ for_emoji.py
@@ -0,0 +1,4 @@
+#U+1F438	
+emoji = '\U0001F438'
+for n in range(1,10):
+    print(emoji* n)
